996d5862465c453db2ab2b0ae8dce6766bf4e3b4
Emile <emile.app@gmail.com>
2017-06-25 00:49:37 +0200

    changed to point to production mongo db... should use an ENV variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 console.log(".... SETUP MONGODB ....");
 mongoose.Promise = global.Promise;// tell mongoose that we're using promises not callbacks... only do thins once here
-mongoose.connect('mongodb://localhost:27017/RSS', function(err, db) {
+mongoose.connect('mongodb://mongo:27017/RSS', function(err, db) {
     if (err) {
         console.log('Unable to connect to the server. Please start the server. Error:', err);
     } else {
